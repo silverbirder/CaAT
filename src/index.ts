ec1b55b668562148946d1ea13687d98ae8e8763f
shibamoto <silverbirder@gmail.com>
2020-01-18 23:35:29 +0900

    Not use the RegExp option (global)

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import GroupImpl from "./group/groupImpl";
 
 function sampleMember() {
     const member: IMember = new MemberImpl('XXXX');
-    member.ignore = new RegExp('YYYY', 'ig');
+    member.ignore = new RegExp('YYYY', 'i');
     member.everyMinutes = 15;
     member.cutTimeRange = [{from: new Date('2020-01-18T12:00'), to: new Date('2020-01-18T13:00')}];
     member.startDate = new Date('2020-01-18');
@@ -15,13 +15,19 @@ function sampleMember() {
 
 function sampleGroup() {
     const group: iGroup = new GroupImpl('XXXX');
-    group.holidayWords.all = new RegExp('X', 'ig');
-    group.holidayWords.morning = new RegExp('XX', 'ig');
-    group.holidayWords.afternoon = new RegExp('XX', 'ig');
-    const members: Array<IGroupMember> = [{
-        name: 'XXXX',
-        match: new RegExp('XXXX', 'ig')
-    }];
+    group.holidayWords.all = new RegExp('休', 'i');
+    group.holidayWords.morning = new RegExp('AM', 'i');
+    group.holidayWords.afternoon = new RegExp('PM', 'i');
+    const members: Array<IGroupMember> = [
+        {
+            name: 'XX',
+            match: new RegExp('XX', 'i')
+        },
+        {
+            name: 'XX',
+            match: new RegExp('XX', 'i')
+        }
+    ];
     group.members = members;
     group.startDate = new Date('2020-01-18');
     group.endDate = new Date('2020-01-18');
