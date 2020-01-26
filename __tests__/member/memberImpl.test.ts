c232680a1b546a830a6ed91418b1bf9cc4827051
shibamoto <silverbirder@gmail.com>
2020-01-26 15:14:38 +0900

    Add test code

---
+++ __tests__/member/memberImpl.test.ts
@@ -0,0 +1,9 @@
+describe('MemberImpl', ()=> {
+    describe('fetchSchedules', () => {
+        test('None', () => {
+            // const member: IMember = new MemberImpl('user@gmail.com');
+            // const schedules: Array<ISchedule> = member.fetchSchedules();
+            // TODO: DIP (CalendarApp.getCalendarById)
+        })
+    })
+});
